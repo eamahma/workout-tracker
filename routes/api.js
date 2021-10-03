5d19ef9c5674196a10d8f521ba440d2d3b544ef6
eamahma <eamahma@gmail.com>
2021-10-02 21:30:10 -0400

    routes for api/workouts/range & /stats page added

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 router.get('/api/workouts', async (req, res) => {
     try {
-        let allWorkouts = await Workout.aggregate([{
+        let lastWorkouts = await Workout.aggregate([{
             $addFields:{
                 totalDuration: {
                     $sum: "$exercises.duration"
@@ -12,10 +12,32 @@ router.get('/api/workouts', async (req, res) => {
             }
         }])
 
-        res.json(allWorkouts)
+        res.json(lastWorkouts)
     } catch (err) {
         res.status(500).json(err.message)
     }
+})
+
+
+router.get('/api/workouts/range', async (req, res) => {
+    try {
+        let rangeWorkouts = await Workout.aggregate([{
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            }
+        }]).sort({
+           _id: -1 
+        }).limit(7);
+        res.json(rangeWorkouts)
+    } catch(err) {
+        res.status(500).json(err.message)
+    }
+})
+
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname,'../public/stats.html'))
 })
 
 module.exports = router;
